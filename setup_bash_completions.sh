113be8a1376cb0b086a5f646193d4fba46137557
정시원 <sebastianrcnt@yonsei.ac.kr>
2024-07-17 15:01:33 +0900

    Update setup_bash_completions.sh

--- setup_bash_completions.sh
+++ setup_bash_completions.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# bash should load .bashrc.
+# if not, add 'source .bashrc' at the end of .bash_profile
